284293a351bdd47c68b58850a38af1d137a0dd42
문일윤 <iym1511@naver.com>
2024-02-01 21:05:40 +0900

    template.tsx, Link, Image, redirect

---
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+const Login = () => {
+  <div>
+    로그인페이지
+  </div>
+};
+
+export default Login;
---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from "next/navigation";
+
+// 리다이렉트 되는곳
+const Login = () => {
+  redirect('i/flow/login');
+};
+
+export default Login;
