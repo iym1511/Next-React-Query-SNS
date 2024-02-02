779884ffafe395d3279836740e4cfafeb4c1d659
문일윤 <iym1511@naver.com>
2024-02-02 20:17:17 +0900

    default.tsx

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -13,3 +13,6 @@ const Layout = ({children, modal}: Prop) => {
 }
 
 export default Layout;
+
+// 주소가 localhost:3001일 때는 children-> page.tsx, modal->@modal/default.tsx
+// 주소가 localhost:3001/i/flow/login 때는 children -> i/flow/login/page.tsx,  modal->@modal/i/flow/login/page.tsx
