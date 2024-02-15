815492357375a1c45c21a953b14e37aa93a366e1
문일윤 <iym1511@naver.com>
2024-02-15 15:22:00 +0900

    이벤트 캡처링과 /status/[id] 페이지

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,1 +1,31 @@
 // 한 사람의 개별 게시글
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import style from './singlePost.module.css';
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
+
+export default function SinglePost() {
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton/>
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      {/* 원본 게시글 */}
+      <Post />
+      <CommentForm />
+      <div>
+        {/* 원본게시글의 답글 */}
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  )
+}
--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -4,7 +4,7 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from './ActionButtons';
-// import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import PostArticle from './PostArticle';
 
 // 한글 플러그인
 dayjs.locale('ko');
@@ -12,6 +12,7 @@ dayjs.extend(relativeTime)
 
 export default function Post() {
   const target = {
+    postId:1,
     User: {
       id: 'elonmusk',
       nickname: 'Elon Musk',
@@ -23,7 +24,7 @@ export default function Post() {
   }
   
   return (
-    <article className={style.post}>
+    <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
@@ -50,6 +51,6 @@ export default function Post() {
           <ActionButtons />
         </div>
       </div>
-    </article>
+    </PostArticle>
   )
 }
