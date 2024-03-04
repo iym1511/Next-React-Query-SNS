2f528cde428d868d27b770af170230ae97bf048f
문일윤 <iym1511@naver.com>
2024-03-04 20:52:04 +0900

    react-query를 쓰는 이유와 fresh, stale, inactive

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -10,6 +10,8 @@ const PostRecommends = () => {
   const { data } = useQuery<IPost[]>({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
+    staleTime : 60 * 1000, // n분 뒤에 fresh -> stale 로
+    gcTime : 300 * 1000 // 5분뒤 메모리 정리
   });
   return data?.map((post) => (
     <Post key={post.postId} post={post}/>
