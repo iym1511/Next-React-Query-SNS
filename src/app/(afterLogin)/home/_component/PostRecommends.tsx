692adcdc5b3d396350ee70e8322ffe462d3fa37c
문일윤 <iym1511@naver.com>
2024-03-19 19:55:47 +0900

    인피니트 스크롤링

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,21 +1,45 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import {
+  InfiniteData,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
 import { getPostRecommends } from "../_lib/getPostRecommends";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { User } from "@/model/User";
 import { Post as IPost } from "@/model/Post";
+import { Fragment } from "react";
 
 const PostRecommends = () => {
-  const { data } = useQuery<IPost[]>({
+  const { data } = useInfiniteQuery<
+    IPost[],
+    Object,
+    InfiniteData<IPost[]>,
+    [_1: string, _2: string],
+    number
+  >({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
-    staleTime : 60 * 1000, // n분 뒤에 fresh -> stale 로
-    gcTime : 300 * 1000, // 5분뒤 메모리 정리
+    initialPageParam: 0, // 2차원 배열로 담겨 있다 [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15]]
+    getNextPageParam: (lastPage) => lastPage.at(-1)?.postId, // 최근에 불러왔던 데이터 5개 마지막 페이지에 postId
+    staleTime: 60 * 1000, // n분 뒤에 fresh -> stale 로
+    gcTime: 300 * 1000, // 5분뒤 메모리 정리
   });
-  return data?.map((post) => (
-    <Post key={post.postId} post={post}/>
-  ));
+
+  // prop이 있으면 2차원 배열에 map의 key를 넣을때 Fragment사용
+  return (
+    <>
+      {data?.pages.map((page, i) => (
+        <Fragment key={i}>
+          {page.map((post) => (
+            <Post key={post.postId} post={post} />
+          ))}
+        </Fragment>
+      ))}
+    </>
+  );
+  
 };
 
 export default PostRecommends;
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,13 +2,11 @@ import style from "./home.module.css";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
-import Post from "@/app/(afterLogin)/_component/Post";
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
-import PostRecommends from "@/app/(afterLogin)/home/_component/PostRecommends";
 import { getPostRecommends } from "./_lib/getPostRecommends";
 import TabDecider from "./_component/TabDecider";
 
@@ -16,9 +14,10 @@ import TabDecider from "./_component/TabDecider";
 const Home = async () => {
 
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
+  await queryClient.prefetchInfiniteQuery({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
+    initialPageParam : 0
   });
   const dehydratedState = dehydrate(queryClient);
 
