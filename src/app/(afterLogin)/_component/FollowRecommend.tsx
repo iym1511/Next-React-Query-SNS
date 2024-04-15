02bd0c4123a293978774fdce32702956dcc1f6e8
문일윤 <iym1511@naver.com>
2024-04-15 22:12:48 +0900

    프로필 데이터 형태 확인

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import style from './followRecommend.module.css';
 import { User } from '@/model/User';
 
@@ -9,8 +9,64 @@ type Prop = {
 }
 
 export default function FollowRecommend({user}:Prop) {
+  const followed = false;
+  const queryClient = useQueryClient();
 
-  const onFollow = () => {};
+  const follow = useMutation({
+    mutationFn : (userId:string) => {
+      return  fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/${userId}/follow`,{
+        method:'post',
+        credentials:'include'
+      });
+    },
+    onMutate() {
+      // const queryCache = queryClient.getQueryCache();
+      // const queryKeys = queryCache.getAll().map((cache) => cache.queryKey);
+    },
+    onError() {
+
+    }
+  })
+  
+  const unfollow = useMutation({
+    mutationFn : (userId: string) => {
+      return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/${userId}/follow`,{
+        method:'delete',
+        credentials:'include'
+      });
+
+    },
+    onMutate(userId: string) {
+      const value:User[] | undefined = queryClient.getQueryData(['users', 'followRecommends']);
+      if(value){
+        const index = value.findIndex((v) => v.id === userId);
+        const shallow = [...value]
+      
+        shallow[index] = {
+          ...shallow[index],
+          _count: {
+            // 수정 요망 (내가적은거)
+            Followers : shallow[index]._count.Followers + 1,
+            Followings : shallow[index]._count.Followings + 1,
+          }
+
+        };
+        console.log("index : ",index, "shallow",shallow);
+      }
+      queryClient.setQueryData(['users', 'followRecommends'], value);
+    },
+    onError() {
+      
+    }
+  })
+
+  const onFollow = () => {
+    if(followed){
+      unfollow.mutate();
+    }else{
+      follow.mutate();
+    }
+  };
 
   return (
     <div className={style.container}>
--- src/model/User.ts
+++ src/model/User.ts
@@ -2,4 +2,8 @@ export interface User {
   id: string;
   nickname: string;
   image: string;
+  _count: {
+    Followers: number,
+    Followings: number
+  }
 }
