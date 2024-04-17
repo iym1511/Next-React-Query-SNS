aad37e61b3ed7fa1f28f7ecc78f0ac36f8c49330
문일윤 <iym1511@naver.com>
2024-04-17 14:30:01 +0900

    팔로우/언팔로우 optimistic update

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -3,25 +3,49 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import style from './followRecommend.module.css';
 import { User } from '@/model/User';
+import { useSession } from 'next-auth/react';
+import cx from 'classnames';
 
 type Prop = {
   user : User
 }
 
 export default function FollowRecommend({user}:Prop) {
-  const followed = false;
+
   const queryClient = useQueryClient();
+  const {data : session} = useSession();
+  const followed = !!user.Followers?.find((v) => v.userId === session?.user?.email);
 
   const follow = useMutation({
     mutationFn : (userId:string) => {
-      return  fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/${userId}/follow`,{
-        method:'post',
-        credentials:'include'
-      });
+      return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userId}/follow`, {
+        credentials: 'include',
+        method: 'post',
+      })
     },
-    onMutate() {
-      // const queryCache = queryClient.getQueryCache();
-      // const queryKeys = queryCache.getAll().map((cache) => cache.queryKey);
+    onMutate(userId : string) {
+      /** 데이터 가져오기 */
+      const value:User[] | undefined = queryClient.getQueryData(['users', 'followRecommends']);
+      if(value){
+        const index = value.findIndex((v) => v.id === userId);
+        /** 변경된 값 */
+        const shallow = [...value]
+      
+        shallow[index] = {
+          ...shallow[index],
+            Followers : [
+              {
+                userId : session?.user?.email as string,
+              }
+            ],
+            _count : {
+              ...shallow[index]._count,
+              Followers : shallow[index]._count?.Followers +1,
+            }
+        };
+        console.log("index : ",index, "shallow",shallow);
+        queryClient.setQueryData(['users', 'followRecommends'], shallow);
+      }
     },
     onError() {
 
@@ -37,34 +61,38 @@ export default function FollowRecommend({user}:Prop) {
 
     },
     onMutate(userId: string) {
+      /** 데이터 가져오기 */
       const value:User[] | undefined = queryClient.getQueryData(['users', 'followRecommends']);
+      
       if(value){
         const index = value.findIndex((v) => v.id === userId);
+        /** 변경된 값 */
         const shallow = [...value]
-      
+ 
         shallow[index] = {
           ...shallow[index],
-          _count: {
-            // 수정 요망 (내가적은거)
-            Followers : shallow[index]._count.Followers + 1,
-            Followings : shallow[index]._count.Followings + 1,
-          }
-
+            Followers : shallow[index].Followers.filter((v)=> v.userId !== session?.user?.email), // 본인 제거
+            _count : {
+              ...shallow[index]._count,
+              Followers : shallow[index]._count?.Followers - 1,
+            }
         };
         console.log("index : ",index, "shallow",shallow);
+        queryClient.setQueryData(['users', 'followRecommends'], shallow);
       }
-      queryClient.setQueryData(['users', 'followRecommends'], value);
     },
-    onError() {
-      
+    onError(error) {
+      console.error(error);
     }
   })
 
   const onFollow = () => {
+    console.log("followed : ",followed)
     if(followed){
-      unfollow.mutate();
+      // useMutation에서 매게변수로 쓰이는 인자 전달.
+      unfollow.mutate(user.id);
     }else{
-      follow.mutate();
+      follow.mutate(user.id);
     }
   };
 
@@ -79,8 +107,8 @@ export default function FollowRecommend({user}:Prop) {
         <div className={style.title}>{user.nickname}</div>
         <div className={style.count}>@{user.id}</div>
       </div>
-      <div className={style.followButtonSection}>
-        <button onClick={onFollow}>팔로우</button>
+      <div className={cx(style.followButtonSection, followed && style.followed)}>
+        <button onClick={onFollow}>{followed ? '팔로잉' : '팔로우'}</button>
       </div>
     </div>
   )
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,9 +1,13 @@
+export interface UserID {
+  userId: string
+}
 export interface User {
+  Followers : UserID[];
   id: string;
   nickname: string;
   image: string;
   _count: {
-    Followers: number,
-    Followings: number
+    Followers: number;
+    Followings: number;
   }
 }
