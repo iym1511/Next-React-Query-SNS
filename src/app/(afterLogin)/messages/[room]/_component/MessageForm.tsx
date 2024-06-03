55fee4cfea072c7716df06e49cbc37c4acef67ea
문일윤 <iym1511@naver.com>
2024-06-03 22:16:07 +0900

    FEAT : 채팅창이 바로 업데이트 되는 로직 추가

--- src/app/(afterLogin)/messages/[room]/_component/MessageForm.tsx
+++ src/app/(afterLogin)/messages/[room]/_component/MessageForm.tsx
@@ -4,6 +4,8 @@ import TextareaAutosize from "react-textarea-autosize";
 import {ChangeEvent, FormEvent, useEffect, useState} from "react";
 import useSocket from '../_lib/useSocket';
 import { useSession } from 'next-auth/react';
+import { InfiniteData, useQueryClient } from '@tanstack/react-query';
+import { Message } from '@/model/Message';
 
 interface Props {
   id : string
@@ -15,6 +17,7 @@ const MessageForm = ({ id }:Props) => {
   // 이것만 추가하면 소켓 연결이 됨
   const [socket] = useSocket();
   const { data: session } = useSession();
+  const queryClient = useQueryClient();
 
   const onChangeContent = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
@@ -22,6 +25,11 @@ const MessageForm = ({ id }:Props) => {
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if(!session?.user?.email){
+      return;
+    }
+    const ids = [session.user.email, id];
+    ids.sort();
     // soket.io
     // 서버에서의 약속 'sendMessage' 로 보내면 실시간으로 채팅이 간다는 이벤트 이름
     socket?.emit('sendMessage', {
@@ -29,6 +37,32 @@ const MessageForm = ({ id }:Props) => {
       receiverId : id,
       content,
     });
+
+    // 리액트 쿼리 데이터에 추가
+    const exMessages = queryClient.getQueryData(['rooms', {senderId: session?.user?.email, receiverId: id} ,'messages']) as InfiniteData<Message[]>;
+    if(exMessages && typeof exMessages === "object"){ // exMessages가 있고 타입검사 확인
+      // exMessages 객체에서 pages 부분만 업데이트
+      const newMessages = {
+        ...exMessages,
+        pages: [
+          ...exMessages.pages
+        ],
+      };
+
+      const lastPage = newMessages.pages.at(-1); // 배열 마지막 부분(채팅배열이 위치한 곳)
+      const newLastPage = lastPage ? [...lastPage] : []; 
+      let lastMessageId = lastPage?.at(-1)?.messageId; // 채팅배열 마지막부분 id
+      newLastPage.push({
+        senderId: session.user.email,
+        receiverId: id,
+        content,
+        room: ids.join('-'), // 배열 마디를 - 로 연결
+        messageId: lastMessageId ? lastMessageId + 1 : 1, // 메시지 아이디 증가
+        createdAt: new Date(),
+      });
+      newMessages.pages[newMessages.pages.length - 1] = newLastPage; // 채팅배열쪽에 추가
+      queryClient.setQueryData(['rooms', {senderId: session?.user?.email, receiverId: id} ,'messages'], newMessages); // 채팅 저장
+    }
     setContent('');
   }
 
